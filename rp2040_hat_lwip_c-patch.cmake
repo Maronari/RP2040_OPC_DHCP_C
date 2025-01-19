9dbf737c05faa2b56d440160cdc01e111ade0696
Ivan Korsunov <ivan.corsunov@gmail.com>
2025-01-19 18:49:52 +0300

    Migrate to RP2350

--- rp2040_hat_lwip_c-patch.cmake
+++ rp2040_hat_lwip_c-patch.cmake
@@ -18,12 +18,12 @@ set(PICO_SDK_TINYUSB_SRC_DIR "${PICO_SDK_SRC_DIR}/lib/tinyusb")
 set(RP2040_HAT_C_PATCH_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/patches")
 
 # Delete untracked files in ioLibrary_Driver
-if(EXISTS "${IOLIBRARY_DRIVER_SRC_DIR}/.git")
-	message("cleaning ioLibrary_Driver...")
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} clean -fdx)
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} reset --hard)
-	message("ioLibrary_Driver cleaned")
-endif()
+# if(EXISTS "${IOLIBRARY_DRIVER_SRC_DIR}/.git")
+# 	message("cleaning ioLibrary_Driver...")
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} clean -fdx)
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} reset --hard)
+# 	message("ioLibrary_Driver cleaned")
+# endif()
 
 # Delete untracked files in pico-sdk
 # if(EXISTS "${PICO_SDK_SRC_DIR}/.git")
@@ -33,22 +33,22 @@ endif()
 # 	message("pico-sdk cleaned")
 # endif()
 
-execute_process(COMMAND ${GIT_EXECUTABLE} -C ${RP2040_HAT_LWIP_C_SRC_DIR} submodule update --init)
+# execute_process(COMMAND ${GIT_EXECUTABLE} -C ${RP2040_HAT_LWIP_C_SRC_DIR} submodule update --init)
 
-# Delete untracked files in lwip
-if(EXISTS "${PICO_SDK_LWIP_SRC_DIR}/.git")
-	message("cleaning pico-sdk lwip...")
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} clean -fdx)
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} reset --hard)
-	message("pico-sdk lwip cleaned")
-endif()
+# # Delete untracked files in lwip
+# if(EXISTS "${PICO_SDK_LWIP_SRC_DIR}/.git")
+# 	message("cleaning pico-sdk lwip...")
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} clean -fdx)
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} reset --hard)
+# 	message("pico-sdk lwip cleaned")
+# endif()
 
-# Delete untracked files in tinyusb
-if(EXISTS "${PICO_SDK_TINYUSB_SRC_DIR}/.git")
-	message("cleaning pico-sdk tinyusb...")
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_TINYUSB_SRC_DIR} clean -fdx)
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_TINYUSB_SRC_DIR} reset --hard)
-	message("pico-sdk tinyusb cleaned")
-endif()
+# # Delete untracked files in tinyusb
+# if(EXISTS "${PICO_SDK_TINYUSB_SRC_DIR}/.git")
+# 	message("cleaning pico-sdk tinyusb...")
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_TINYUSB_SRC_DIR} clean -fdx)
+# 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_TINYUSB_SRC_DIR} reset --hard)
+# 	message("pico-sdk tinyusb cleaned")
+# endif()
 
-execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_SRC_DIR} submodule update --init)
+# execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_SRC_DIR} submodule update --init)
